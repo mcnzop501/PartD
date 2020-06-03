ada98471d93ef33396140c5c7e45bb6436a16dad
mcnzop501 <mcnzop501@gmail.com>
2020-06-03 19:03:26 +1200

    Fourth line of text added to main class.

--- Simple Application/src/Main.java
+++ Simple Application/src/Main.java
@@ -5,6 +5,7 @@ public class Main {
 		System.out.println("Hi, my name is Matt.");
 		System.out.println("My favourite colour is black.");
 		System.out.println("My favourite mode of transportation is biking.");
+		System.out.println("The best day of the week is Friday.");
 		
 	}
 }
